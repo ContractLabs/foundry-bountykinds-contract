f47640aafa1d9210657ef2cb7a12eba6c2ed39fa
tasibii <tasibii.web3@gmail.com>
2023-12-15 13:19:27 +0700

    update testnet

--- src/new-testnet/BKTreasury.sol
+++ src/new-testnet/BKTreasury.sol
@@ -2,32 +2,31 @@
 pragma solidity 0.8.20;
 
 // forgefmt: disable-start
-import {
-    IBKTreasury
-} from "src/interfaces/IBKTreasury.sol";
+import {DateTimeLib} from "src/libraries/DateTimeLib.sol";
 
-import {
-    EnumerableSet
-} from "src/oz-custom/oz/utils/structs/EnumerableSet.sol";
+import {IBKTreasury} from "src/interfaces/IBKTreasury.sol";
 
-import {
-    Roles,
-    IAuthority,
-    TreasuryUpgradeable
-} from "src/oz-custom/presets-upgradeable/TreasuryUpgradeable.sol";
+import {EnumerableSet} from "src/oz-custom/oz/utils/structs/EnumerableSet.sol";
 
-import {
-    AggregatorV3Interface
-} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import {Roles, IAuthority, ERC165CheckerUpgradeable, IERC1155, TreasuryUpgradeable} from "src/oz-custom/presets-upgradeable/TreasuryUpgradeable.sol";
 // forgefmt: disable-end
 
 contract BKTreasury is TreasuryUpgradeable, IBKTreasury {
+    using ERC165CheckerUpgradeable for address;
     using EnumerableSet for EnumerableSet.AddressSet;
 
     AggregatorV3Interface public priceFeed;
-
     mapping(address => uint256) private __priceOf;
     EnumerableSet.AddressSet private __supportedPayments;
+
+    mapping(address => uint256) public limitTokenPerUser;
+    mapping(address => uint256) public limitTokenPerDay;
+    mapping(address => mapping(uint256 => uint256)) public
+        tokenWithdrawedAmountPerDay;
+    mapping(address => mapping(address => mapping(uint256 => uint256))) public
+        userWithdrawedAmountPerDay;
 
     function initialize(
         string calldata name_,
@@ -106,5 +105,69 @@ contract BKTreasury is TreasuryUpgradeable, IBKTreasury {
         return __supportedPayments.values();
     }
 
-    uint256[47] private __gap;
+    function setLimit(
+        address token_,
+        uint256 limitPerUser_,
+        uint256 limitTokenPerDay_
+    )
+        external
+        onlyRole(Roles.OPERATOR_ROLE)
+    {
+        limitTokenPerDay[token_] = limitTokenPerDay_;
+        limitTokenPerUser[token_] = limitPerUser_;
+    }
+
+    function _configTimestamp(uint256 rawTimestamp_)
+        internal
+        pure
+        returns (uint256 configuredTimestamp)
+    {
+        (uint256 year, uint256 month, uint256 day,,,) =
+            DateTimeLib.timestampToDateTime(rawTimestamp_);
+
+        configuredTimestamp = DateTimeLib._daysFromDate(year, month, day)
+            * DateTimeLib.SECONDS_PER_DAY;
+    }
+
+    function _beforeWithdraw(
+        address token_,
+        address to_,
+        uint256 value_,
+        uint256 amount_ // for erc1155
+    )
+        internal
+        override
+    {
+        uint256 timestamp = _configTimestamp(block.timestamp);
+
+        uint256 _limitTokenPerDay = limitTokenPerDay[token_];
+        uint256 _limitTokenPerUser = limitTokenPerUser[token_];
+
+        uint256 currentTokenWithdrawedAmount =
+            tokenWithdrawedAmountPerDay[token_][timestamp];
+        uint256 currentUserWithdrawedAmount =
+            userWithdrawedAmountPerDay[token_][to_][timestamp];
+
+        if (token_.supportsInterface(type(IERC1155).interfaceId)) {
+            if ((currentTokenWithdrawedAmount + amount_) > _limitTokenPerDay) {
+                revert("The token exceeded the withdrawal limit.");
+            }
+            if ((currentUserWithdrawedAmount + amount_) > _limitTokenPerUser) {
+                revert("The user exceeded the withdrawal limit.");
+            }
+            tokenWithdrawedAmountPerDay[token_][timestamp] += amount_;
+            userWithdrawedAmountPerDay[token_][to_][timestamp] += amount_;
+        } else {
+            if ((currentTokenWithdrawedAmount + value_) > _limitTokenPerDay) {
+                revert("The token exceeded the withdrawal limit.");
+            }
+            if ((currentUserWithdrawedAmount + value_) > _limitTokenPerUser) {
+                revert("The user exceeded the withdrawal limit.");
+            }
+            tokenWithdrawedAmountPerDay[token_][timestamp] += value_;
+            userWithdrawedAmountPerDay[token_][to_][timestamp] += value_;
+        }
+    }
+
+    uint256[43] private __gap;
 }
