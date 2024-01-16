a1fe8cebf91a7148991323dbef703358006adf07
tasibii <tasibii.web3@gmail.com>
2024-01-16 17:31:34 +0700

    refactor BK20

--- src/interfaces/IBK20.sol
+++ src/interfaces/IBK20.sol
@@ -8,32 +8,6 @@ pragma solidity 0.8.20;
  * calls to external contracts.
  */
 interface IBK20 {
-    error BountyKindsERC20__Blacklisted();
-    error BountyKindsERC20__InvalidArguments();
-
-    /**
-     * @dev Emitted when a refund is made to an operator.
-     * @param operator The address of the operator that receives the refund.
-     * @param refund The amount of tokens refunded.
-     */
-    event Refunded(address indexed operator, uint256 indexed refund);
-
-    /**
-     * @dev Emitted when a call to an external contract is executed.
-     * @param operator The address of the operator that executed the call.
-     * @param target The address of the external contract that was called.
-     * @param value_ The amount of ether sent in the call.
-     * @param callData The data sent in the call.
-     * @param returnData The data returned by the external contract.
-     */
-    event Executed(
-        address indexed operator,
-        address indexed target,
-        uint256 indexed value_,
-        bytes callData,
-        bytes returnData
-    );
-
     /**
      * @dev Mints tokens to the specified address.
      * @param to_ The address that will receive the tokens.
@@ -42,15 +16,9 @@ interface IBK20 {
     function mint(address to_, uint256 amount_) external;
 
     /**
-     * @dev Executes a call to an external contract.
-     * @param target_ The address of the external contract to call.
-     * @param value_ The amount of ether to send with the call.
-     * @param calldata_ The data to send with the call.
+     * @dev Burns tokens to the specified address.
+     * @param from_ The address that will burn the tokens.
+     * @param amount_ The amount of tokens to burn.
      */
-    function execute(
-        address target_,
-        uint256 value_,
-        bytes calldata calldata_
-    )
-        external;
+    function burn(address from_, uint256 amount_) external;
 }
--- src/mainnet/BK20.sol
+++ src/mainnet/BK20.sol
@@ -5,70 +5,38 @@ pragma solidity 0.8.20;
 import {
     IBK20
 } from "../interfaces/IBK20.sol";
-
 import {
-    ErrorHandler
-} from "../oz-custom/libraries/ErrorHandler.sol";
-
+    ERC165CheckerUpgradeable,
+    BKFundForwarderUpgradeable
+} from "../internal-upgradeable/BKFundForwarderUpgradeable.sol";
 import {
-    UUPSUpgradeable
-} from "../oz-custom/oz-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-
-import {
-    TransferableUpgradeable
-} from "../oz-custom/internal-upgradeable/TransferableUpgradeable.sol";
-
-import {
-    ProxyCheckerUpgradeable
-} from "../oz-custom/internal-upgradeable/ProxyCheckerUpgradeable.sol";
-
-import {
-    BlacklistableUpgradeable
-} from "../oz-custom/internal-upgradeable/BlacklistableUpgradeable.sol";
-
-import {
-    AccessControlEnumerableUpgradeable
-} from "../oz-custom/oz-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
-
+    Roles,
+    IAuthority,
+    ManagerUpgradeable
+} from "../oz-custom/presets-upgradeable/base/ManagerUpgradeable.sol";
+import { 
+    IFundForwarderUpgradeable 
+} from "../oz-custom/internal-upgradeable/interfaces/IFundForwarderUpgradeable.sol";
 import {
     ERC20Upgradeable,
     ERC20PermitUpgradeable
 } from "../oz-custom/oz-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-
 import {
     ERC20BurnableUpgradeable
 } from "../oz-custom/oz-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-
-import {
-    PausableUpgradeable,
-    ERC20PausableUpgradeable
-} from "../oz-custom/oz-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
 // forgefmt: disable-end
 
 contract BK20 is
     IBK20,
-    UUPSUpgradeable,
+    ERC20Upgradeable,
+    ManagerUpgradeable,
     ERC20PermitUpgradeable,
-    TransferableUpgradeable,
-    ProxyCheckerUpgradeable,
-    BlacklistableUpgradeable,
     ERC20BurnableUpgradeable,
-    ERC20PausableUpgradeable,
-    AccessControlEnumerableUpgradeable
+    BKFundForwarderUpgradeable
 {
-    using ErrorHandler for bool;
-
-    bytes32 public constant PAUSER_ROLE =
-        0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a;
-    bytes32 public constant MINTER_ROLE =
-        0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6;
-    bytes32 public constant OPERATOR_ROLE =
-        0x97667070c54ef182b0f5858b034beac1b6f3089aa2d3188bb1e8929f4fa9b929;
-    bytes32 public constant UPGRADER_ROLE =
-        0x189ab7a9244df0848122154315af71fe140f3db0fe014031783b0946b8c9d2e3;
-
     function initialize(
-        address admin_,
+        IAuthority authority_,
+        address recipient_,
         uint256 initialSupply_,
         string calldata name_,
         string calldata symbol_
@@ -76,45 +44,10 @@ contract BK20 is
         external
         initializer
     {
-        __UUPSUpgradeable_init();
-        __ERC20Permit_init(name_);
         __ERC20_init_unchained(name_, symbol_);
-        address operator = _msgSender();
-
-        bytes32 pauserRole = PAUSER_ROLE;
-        bytes32 minterRole = MINTER_ROLE;
-        bytes32 operatorRole = OPERATOR_ROLE;
-        bytes32 upgraderRole = UPGRADER_ROLE;
-
-        _grantRole(pauserRole, operator);
-        _grantRole(minterRole, operator);
-        _grantRole(operatorRole, operator);
-        _grantRole(upgraderRole, operator);
-
-        _grantRole(pauserRole, admin_);
-        _grantRole(minterRole, admin_);
-        _grantRole(operatorRole, admin_);
-        _grantRole(DEFAULT_ADMIN_ROLE, admin_);
-
-        _mint(admin_, initialSupply_ * 1 ether);
-    }
-
-    function setUserStatus(
-        address account_,
-        bool status_
-    )
-        external
-        onlyRole(OPERATOR_ROLE)
-    {
-        _setUserStatus(account_, status_);
-    }
-
-    function pause() external onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() external onlyRole(PAUSER_ROLE) {
-        _unpause();
+        __ERC20Permit_init(name_);
+        __Manager_init_unchained(authority_, 0);
+        _mint(recipient_, initialSupply_ * 1 ether);
     }
 
     /// @inheritdoc IBK20
@@ -123,30 +56,40 @@ contract BK20 is
         uint256 amount_
     )
         external
-        onlyRole(MINTER_ROLE)
+        onlyRole(Roles.MINTER_ROLE)
     {
         _mint(to_, amount_);
     }
 
-    /// @inheritdoc IBK20
-    //  @dev minimal function to recover lost funds
-    function execute(
-        address target_,
-        uint256 value_,
-        bytes calldata calldata_
-    )
+    /**
+     * @dev Allows only the treasurer to change the vault address.
+     *
+     * @param vault_ The new vault address.
+     */
+    function changeVault(address vault_)
         external
-        whenPaused
-        onlyRole(OPERATOR_ROLE)
+        override
+        onlyRole(Roles.TREASURER_ROLE)
     {
-        (bool success, bytes memory returnOrRevertData) =
-            target_.call{ value: value_ }(calldata_);
-        success.handleRevertIfNotSuccess(returnOrRevertData);
-
-        emit Executed(
-            _msgSender(), target_, value_, calldata_, returnOrRevertData
-        );
+        _changeVault(vault_);
     }
+
+    function _beforeRecover(bytes memory)
+        internal
+        override
+        whenPaused
+        onlyRole(Roles.OPERATOR_ROLE)
+    { }
+
+    function _afterRecover(
+        address,
+        address,
+        uint256,
+        bytes memory
+    )
+        internal
+        override
+    { }
 
     function _beforeTokenTransfer(
         address from_,
@@ -154,15 +97,9 @@ contract BK20 is
         uint256 amount_
     )
         internal
-        override(ERC20Upgradeable, ERC20PausableUpgradeable)
+        override(ERC20Upgradeable)
+        onlyWhitelisted
     {
-        if (
-            isBlacklisted(to_) || isBlacklisted(from_)
-                || isBlacklisted(_msgSender())
-        ) {
-            revert BountyKindsERC20__Blacklisted();
-        }
-
         super._beforeTokenTransfer(from_, to_, amount_);
     }
 
@@ -170,15 +107,8 @@ contract BK20 is
         public
         view
         virtual
-        override(AccessControlEnumerableUpgradeable, ERC20Upgradeable)
+        override(ERC20Upgradeable)
         returns (bool)
-    { }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        virtual
-        override
-        onlyRole(UPGRADER_ROLE)
     { }
 
     uint256[50] private __gap;
